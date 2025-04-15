15df1f5326f89c2b2c52897c2b67c1448be8e44b
Nikhil Rathod <103273630+nikhilrxthod@users.noreply.github.com>
2025-04-15 13:17:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,23 +4,16 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 def rot13(text):
-    result = []
-    for char in text:
-        if 'a' <= char <= 'z':
-            result.append(chr((ord(char) - ord('a') + 13) % 26 + ord('a')))
-        elif 'A' <= char <= 'Z':
-            result.append(chr((ord(char) - ord('A') + 13) % 26 + ord('A')))
-        else:
-            result.append(char)
-    return ''.join(result)
+    return text.translate(str.maketrans(
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+        "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
+    ))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        input_text = request.form['input_text']
-        output_text = rot13(input_text)
-        return render_template('index.html', output_text=output_text, input_text=input_text)
-    return render_template('index.html', output_text='', input_text='')
+    input_text = request.form.get('input_text', '')
+    output_text = rot13(input_text) if request.method == 'POST' else ''
+    return render_template('index.html', output_text=output_text, input_text=input_text)
 
 if __name__ == '__main__':
     app.run(debug=True)
